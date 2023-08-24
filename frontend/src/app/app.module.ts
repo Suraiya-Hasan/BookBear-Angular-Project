ffdfd300907de2f0e1ef6483531075a96f54b359
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-24 19:27:52 +0600

    finishing the cart page with cart functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { BookPageComponent } from './components/pages/book-page/book-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
     SearchComponent,
     BookPageComponent,
     TagsComponent,
-    CartPageComponent
+    CartPageComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
