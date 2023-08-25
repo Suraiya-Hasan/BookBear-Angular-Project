05b197deae0d2e0e6d58233a80efa62ae0f9405f
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-25 12:39:10 +0600

    adding a not-found component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { BookPageComponent } from './components/pages/book-page/book-page.compon
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { TitleComponent } from './components/partials/title/title.component';
     BookPageComponent,
     TagsComponent,
     CartPageComponent,
-    TitleComponent
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
