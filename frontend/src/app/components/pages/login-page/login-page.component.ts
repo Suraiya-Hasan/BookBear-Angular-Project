b472b6dc1f6311cbae04fc30dcd6ca74272db531
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-28 12:24:49 +0600

    adding login and logout functionality

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,15 +12,16 @@ export class LoginPageComponent implements OnInit {
 
   loginForm!: FormGroup;
   isSubmitted = false;
+  returnUrl = '';
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
-
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
   }
   get fc() {
     return this.loginForm.controls;
@@ -28,8 +31,10 @@ export class LoginPageComponent implements OnInit {
     this.isSubmitted = true;
 
     if (this.loginForm.invalid) return;
+    this.userService.login({ email: this.fc.email.value, password: this.fc.password.value }).subscribe(() => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
 
-    alert(`email ${this.fc.email.value}, password ${this.fc.password.value}`)
   }
 
 }
